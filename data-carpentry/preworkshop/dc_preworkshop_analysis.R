e95dc8e93060bdd0b985942c0a6a709c61993710
Kari Jordan <kjordan@datacarpentry.org>
2017-10-13 10:41:48 +0200

    Removed old files and renamed files

--- data-carpentry/preworkshop/dc_preworkshop_analysis.R
+++ data-carpentry/preworkshop/dc_preworkshop_analysis.R
@@ -12,7 +12,7 @@
 getwd()
 
 #K# Load CSV and assign to dataframe.
-#K# data <- read.csv("public_data/dc_preworkshop_surveydata_2016March_2017Feb.csv", header = TRUE)
+data <- read.csv("data.csv", header = TRUE)
 
 
 #B# change path because I put the data in the same folder as the script file, and I use RStudio -> Session -> 
@@ -657,3 +657,62 @@ ggplot(data_data_management_strategy_tally,
 #K#    for those who have a dataset compare to those who do not?
 #K# 2) Are there differences in the 5 level of agreement questions by status and/or
 #K#    discipline.
+
+
+---
+  title: "Analysis of Data Carpentry Pre-Workshop Survey Data"
+author: "Kari L. Jordan, PhD"
+date: "March 1, 2017"
+---
+  
+  ```{r echo = FALSE}
+# this is a setup that controls what all the other chunks do
+library(knitr)
+opts_chunk$set(message = FALSE,
+               warning = FALSE)
+```
+
+
+#Read in the data, I'm using `readr::read_csv` beause I find this has better default settings than `read.csv`. For example, it does not convert character columns to factors.
+
+```{r}
+# Load CSV and assign to dataframe.
+library(readr)
+my_data <- read_csv("public_data/dc_preworkshop_surveydata_2016March_2017Feb.csv")
+```
+
+#Inspect the data, I'm using `dplyr::glimpse` because it gives a nice, easy to read layout.
+
+```{r}
+#Inspect the structure of dataframe and column headers. 
+library(dplyr)
+glimpse(my_data)
+```
+
+Using dplyr to make a tally table, similar to how you used `plyr::count`, and `knitr::kable` to print a nice table
+
+```{r}
+my_data_status_tally <- 
+  my_data %>% 
+  group_by(Status) %>% 
+  tally() %>% 
+  filter(!is.na(Status)) %>% 
+  arrange(desc(n))
+
+# take a look 
+library(knitr)
+kable(my_data_status_tally)
+```
+
+Using ggplot2 to make a plot of the counts, using the above table. I use `reorder` to make the bars in order from big to small, I use `coord_flip` to turn the plot on its side so we can easily read the x-axis tick labels, and `base_size` to make the font size a little larger. 
+
+```{r}
+library(ggplot2)
+ggplot(my_data_status_tally, 
+       aes(reorder(Status, n),
+           n)) +
+  geom_col() +
+  xlab("Status") +
+  ylab("Count of people") +
+  coord_flip() +
+  theme_bw(base_size = 14)
