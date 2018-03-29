3e0d4f825a476bbcefb6d64867b5b52b102705b8
Kari Jordan <kjordan@datacarpentry.org>
2018-03-28 21:14:21 -0400

    Rendered long term report and figures

--- learner-assessment/archives/2017/code/exploringData.R
+++ learner-assessment/archives/2017/code/exploringData.R
@@ -8,7 +8,7 @@
 #   Copyright (C) 2017 Paula Andrea Martinez
 #   ORCID iD 0000-0002-8990-1985
 
-source(file = "scripts/installpkg.R")
+source(file = "code/installpkg.R")
 
 #####################################
 # download files
