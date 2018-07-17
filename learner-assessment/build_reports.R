1f038c116e0ee7f47cd5e13c4ad93770ce2bdaa9
François Michonneau <francois.michonneau@gmail.com>
2018-07-17 18:40:44 -0400

    add script to build reports

---
+++ learner-assessment/build_reports.R
@@ -0,0 +1,6 @@
+library(rmarkdown)
+
+rmarkdown::render("reports-src/2018-07-learners-pre-post-surveys.Rmd", output_format = "html_document", output_file="../reports/2018-07-learners-pre-post-surveys.html")
+
+
+rmarkdown::render("reports-src/2018-07-learners-pre-post-surveys.Rmd", output_format = "pdf_document", output_file="../reports/2018-07-learners-pre-post-surveys.pdf")
